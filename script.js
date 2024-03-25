55b76b159974c7e46c1bb75990668ea7420730b8
Adelita Martinez <amartinez1013@cnm.edu>
2024-03-25 12:03:56 -0600

    Added some comments to myself

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 let index = 0;
 
+// This is the function being called using onclick
 function changeColors() {
+// Making array of colors
   let colors = ["red", "blue", "orange", "yellow", "green", "purple"]
 
+//
   document.getElementsByTagName("body")[0].
   style.background = colors[index++]
 
