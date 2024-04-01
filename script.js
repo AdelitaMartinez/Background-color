24120af55e97c4b3eb30f73d7119a547059257b1
Adelita Martinez <amartinez1013@cnm.edu>
2024-04-01 10:59:23 -0600

    Added API Ninjas

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 function generate(){
+
+  const apiKey = "GrF0AuxFoZjqTyBqwXeedg==vHKcVvMuZwMynnSd"
+
+  const option = {
+    method: "GET", 
+    headers: {
+      "X-Api-Key": apiKey
+    }
+  }
+
+  const apiURL = "https://api.api-ninjas.com/v1/quotes?category=funny"
+
 let quotes = {
-  "― Mahatma Gandhi": '"“Live as if you were to die tomorrow. Learn as if you were to live forever.”"',
+  "― Mahatma Gandhi": '“Live as if you were to die tomorrow. Learn as if you were to live forever.”',
   "― Albert Einstein": '“There are only two ways to live your life. One is as though nothing is a miracle. The other is as though everything is a miracle.”',
   "― C.S. Lewis": '“You can never get a cup of tea large enough or a book long enough to suit me.”'
 }
@@ -10,8 +22,15 @@ let quotes = {
 
   let quote = quotes[author]
 
-  document.getElementById("quote").innerHTML = quote
+  document.querySelector("#quote").textContent = quote
+  document.querySelector("#author").textContent = author
 
-  document.getElementById("author").innerHTML = author
+  window.onload = function(){
+    generate()
+  }
+
+  // document.getElementById("quote").innerHTML = quote
+
+  // document.getElementById("author").innerHTML = author
 
 }
