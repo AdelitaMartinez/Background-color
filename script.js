65d6d65d645c3f1ed62f2be93c792d05b94cf6f3
Adelita Martinez <amartinez1013@cnm.edu>
2024-04-04 09:11:41 -0600

    Completed project

--- script.js
+++ script.js
@@ -1,48 +1,54 @@
-// // API key for accessing the API
-// const apiKey = "GrF0AuxFoZjqTyBqwXeedg==vHKcVvMuZwMynnSd";
+// API key for accessing the API
+const apiKey = "GrF0AuxFoZjqTyBqwXeedg==vHKcVvMuZwMynnSd";
 
-// // Options for the fetch request, including the API key
-// const options = {
-//   method: "GET",
-//   headers: {
-//     "X-Api-Key": apiKey
-//   }
-// };
+// Options for the fetch request, including the API key
+const options = {
+  method: "GET",
+  headers: {
+    "X-Api-Key": apiKey
+  }
+};
 
-// // URL of the API endpoint for fetching quotes
-// const apiURL = "https://api.api-ninjas.com/v1/quotes?category=funny";
+// URL of the API endpoint for fetching quotes
+const apiURL = "https://api.api-ninjas.com/v1/quotes?category=funny";
 
-// // Fetches quote from API and updates the DOM with quote/author
-// async function generate() {
-//   try {
-//     // Fetch the data from the API
-//     const response = await fetch(apiURL, options);
-//     console.log("Response status:", response.status);
-//     if (!response.ok) {
-//       throw new Error("Network response was not ok");
-//     }
-//     // Parse the response data as JSON
-//     const data = await response.json();
-//     console.log("Response data:", data);
+// Fetches quote from API and updates the DOM with quote/author
+async function generate() {
+  try {
+    // Fetch the data from the API
+    const response = await fetch(apiURL, options);
+    console.log("Response status:", response.status);
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+    // Parse the response data as JSON
+    const data = await response.json();
+    console.log("Response data:", data);
 
-//     // Check if data is available
-//     if (data && data.length > 0) {
-//       const quote = data[0].quote;
-//       const author = data[0].author;
+    // Check if data is available
+    if (data && data.length > 0) {
+      const quote = data[0].quote;
+      const author = data[0].author;
+
+      // Adding quotes around the generated quote
+      const formattedQuote = `"${quote}"`
+
+      // Adding - infront of generated author
+      const formattedAuthor = `- ${author}`
       
 
-//       // Update the DOM with the quote and author
-//       document.querySelector("#quote").textContent = quote;
-//       document.querySelector("#author").textContent = author;
-//     } else {
-//       console.error("No quotes found!");
-//     }
-//   } catch (error) {
-//     // Log any errors that occur dirng fetching or parsing
-//     console.error("Error fetching quotes:", error);
-//   }
-// }
-// // Initialize the page by generating a random quote when the window is loaded
-// window.onload = function () {
-//   generate();
-// };
+      // Update the DOM with the quote and author
+      document.querySelector("#quote").textContent = formattedQuote;
+      document.querySelector("#author").textContent = formattedAuthor;
+    } else {
+      console.error("No quotes found!");
+    }
+  } catch (error) {
+    // Log any errors that occur dirng fetching or parsing
+    console.error("Error fetching quotes:", error);
+  }
+}
+// Initialize the page by generating a random quote when the window is loaded
+window.onload = function () {
+  generate();
+};
